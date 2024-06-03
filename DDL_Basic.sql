b940c29fc610fd3c70e6380fc7c772a5c7fab66c
jihansyamsumar <jihansyams2github@email.com>
2024-06-03 16:18:45 +0700

    Update DDL_Basic.sql

--- DDL_Basic.sql
+++ DDL_Basic.sql
@@ -30,4 +30,41 @@ CREATE TABLE tbl_jobs(
 	max_salary int
 ); 
 
+CREATE TABLE tbl_job_histories(
+	id_employee int,
+	start_date date,
+	end_date date,
+	status varchar(10),
+	job varchar(10),
+	department int
+);
+
+CREATE TABLE tbl_salary(
+	id int,
+	salary_per_hour int,
+	salary_per_day int,
+	working_out int
+	absensi int,
+	month_year date,
+	totalhour int
+);
+
+CREATE TABLE tbl_absensi(
+	id int,
+	id_employee int,
+	clock_in datetime,
+	clock_out datetime,
+	status varchar,
+	time time
+);
+
+CREATE TABLE tbl_salary_history(
+	id_employee int,
+	id_salary int,
+	end_date date,
+	status varchar(10),
+	job varchar(10),
+	department int
+);
+
 /*End Jihan Azzahra*/
