d035867f94d42aeb34a07f00f1327a01aa78a9de
IrhamJ <68208907+IrhamJ@users.noreply.github.com>
2024-06-05 20:38:29 +0700

    commit vw_permission, vw_profile, vw_roles

---
+++ vw_permission.sql
@@ -0,0 +1,6 @@
+USE 
+final_project_employee
+GO
+CREATE VIEW vw_permission AS
+SELECT id, name 
+FROM tbl_permission;
---
+++ vw_roles.sql
@@ -0,0 +1,6 @@
+USE 
+final_project_employee
+GO
+CREATE VIEW vw_role AS
+SELECT id, name 
+FROM tbl_roles;
