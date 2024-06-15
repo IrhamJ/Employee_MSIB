79ab1b51385c14df268c7a2a1c1064462de00562
irvandhar <irvandhars@gmail.com>
2024-06-15 20:57:59 +0700

    Update-dibalik kodenya kebalik

--- SP_update_employee.sql
+++ SP_update_employee.sql
@@ -9,40 +9,19 @@ GO
 -- =============================================
 CREATE PROCEDURE SP_update_employee(
     @id INT,
-    @first_name VARCHAR(25),
-    @last_name VARCHAR(25) = NULL,
-    @gender VARCHAR(10),
-    @email VARCHAR(25),
-    @phone VARCHAR(25) = NULL,
-    @hire_date DATE
+    @job VARCHAR(10)
 )
 AS
 BEGIN
-    DECLARE @Valid_gender BIT;
-    DECLARE @Valid_email BIT;
-    DECLARE @Valid_phone BIT;
-
-    SET @Valid_gender = dbo.func_gender(@gender);
-    SET @Valid_email = dbo.func_email_format(@email);
-    SET @Valid_phone = dbo.func_phone_number(@phone);
-	--Proses validasi gender, email, phone_number
-    IF @Valid_gender = 1 AND @Valid_email = 1 AND @Valid_phone = 1
     BEGIN
         UPDATE tbl_employee
         SET
-            first_name = COALESCE(@first_name, first_name),
-            last_name = COALESCE(@last_name, last_name),
-            gender = COALESCE(@gender, gender),
-            email = COALESCE(@email, email),
-            phone = COALESCE(@phone, phone),
-            hire_date = COALESCE(@hire_date, hire_date)
+            job = COALESCE(@job, job)
         WHERE
             id = @id;
 
-        PRINT 'Data Employee Updated!';
-    END
-    ELSE
-    BEGIN
-        PRINT 'Invalid data!';
+        PRINT 'Employee Job Updated!';
     END
 END;
+
+select * from tbl_employee
--- SP_update_profile.sql
+++ SP_update_profile.sql
@@ -8,71 +8,45 @@ GO
 -- Description: Update data Profil
 -- =============================================
 
-CREATE PROCEDURE SP_update_profile
+CREATE PROCEDURE SP_update_profile(
     @id INT,
     @first_name VARCHAR(25),
-    @last_name VARCHAR(25),
+    @last_name VARCHAR(25) = NULL,
     @gender VARCHAR(10),
     @email VARCHAR(25),
-    @phone VARCHAR(20),
-	@hire_date DATE,
-	@salary INT,
-	@manager INT,
-	@job VARCHAR(10),
-	@department INT
+    @phone VARCHAR(25) = NULL
+)
 AS
 BEGIN
-    SET NOCOUNT ON; -- Mengurangi hasil pengembalian
+    DECLARE @Valid_gender BIT;
+    DECLARE @Valid_email BIT;
+    DECLARE @Valid_phone BIT;
 
-    -- Validasi Gender
-    IF dbo.func_gender (@gender) = '0'
+    SET @Valid_gender = dbo.func_gender(@gender);
+    SET @Valid_email = dbo.func_email_format(@email);
+    SET @Valid_phone = dbo.func_phone_number(@phone);
+
+    --Proses validasi gender, email, phone_number
+    IF @Valid_gender = 1 AND @Valid_email = 1 AND @Valid_phone = 1
     BEGIN
-        PRINT 'Gender input should be Male or Female';
-        RETURN;
-    END
+        UPDATE tbl_employee
+        SET
+            first_name = COALESCE(@first_name, first_name),
+            last_name = COALESCE(@last_name, last_name),
+            gender = COALESCE(@gender, gender),
+            email = COALESCE(@email, email),
+            phone = COALESCE(@phone, phone)
+        WHERE
+            id = @id;
 
-    -- Validasi Email
-    IF dbo.func_email_format(@email) = '0'
-    BEGIN
-        PRINT 'Email should be in the correct format!';
-        RETURN;
-    END
-
-    -- Validasi Phone
-    IF dbo.func_phone_number(@phone) = '0'
-    BEGIN
-        PRINT 'Phone number cannot contain text!';
-        RETURN;
-    END
-
-    -- Update Data Profile
-    UPDATE tbl_employee
-    SET 
-        first_name = COALESCE(@first_name, first_name),
-        last_name = COALESCE(@last_name, last_name),
-        gender = COALESCE(@gender, gender),
-        email = COALESCE(@email, email),
-        phone = COALESCE(@phone, phone),
-		hire_date = COALESCE(@hire_date, hire_date),
-		salary = COALESCE(@salary, salary),
-		manager = COALESCE(@manager, manager),
-		job= COALESCE(@job, job),
-		department = COALESCE(@department, department)
-    WHERE 
-        id = @id;
-
-    DECLARE @rows_affected INT;
-    SET @rows_affected = @@ROWCOUNT;
-
-    IF @rows_affected > 0
-    BEGIN
-        PRINT CAST(@rows_affected AS VARCHAR(10)) + ' row(s) updated successfully.';
+        PRINT 'Data Employee Updated!';
     END
     ELSE
     BEGIN
-        PRINT 'No rows updated.';
+        PRINT 'Invalid data!';
     END
 END;
+
 
 
 -- Menjalankan stored procedure dengan parameter yang diberikan
