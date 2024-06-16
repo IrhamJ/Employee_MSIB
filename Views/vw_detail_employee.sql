8558b7ae9c6f1676fbbc620fa26379460d63db3f
irvandhar <irvandhars@gmail.com>
2024-06-16 09:58:45 +0700

    Update vw_detail_employee.sql

--- Views/vw_detail_employee.sql
+++ Views/vw_detail_employee.sql
@@ -1,13 +1,15 @@
 /*vw_employee_details*/
+/*vw_employee_details*/
 CREATE VIEW vw_employee_details AS
 SELECT 
-	E.id, AC.username, CONCAT(E.first_name,' ',E.last_name) AS full_name, E.gender, E.email, E.hire_date, E.salary,E.manager AS manager_id, 
-	CONCAT(E.first_name,' ',E.last_name) AS manager_name, E.job, E.department, AR.role, L.city, JH.status
+	E.id, A.username, CONCAT(E.first_name,' ',E.last_name) AS full_name, E.gender, E.email, E.hire_date, E.salary,E.manager AS manager_id, 
+	CONCAT(E.first_name,' ',E.last_name) AS manager_name, E.job, D.name, R.role, D.location, H.status
 FROM tbl_employee AS E
-JOIN tbl_account_roles AS AR ON AR.account = E.id
-JOIN tbl_departments AS D ON D.id = E.department
-JOIN tbl_locations AS L ON  L.id = D.location 
-JOIN tbl_account AS AC ON AC.id = E.id
-JOIN tbl_job_histories AS JH ON JH.id_employee = E.id 
+JOIN tbl_account AS A ON E.id = A.id
+JOIN tbl_departments AS D ON E.department = D.id
+JOIN tbl_account_roles AS R ON A.id = R.account
+JOIN tbl_job_histories AS H ON E.id = H.id_employee
+
+
 
 SELECT * FROM vw_employee_details;
